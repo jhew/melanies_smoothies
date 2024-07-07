769dd569d9ea9da69dfd5e15909dd3440a9f300a
Jason Hewitt <jhew@users.noreply.github.com>
2024-07-07 11:38:32 -0400

    add pd df to parse iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,13 @@ st.write('The namme on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+# Convert the Snowpark Dataframe to a Pandas Datafram so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+                                                                      
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -30,6 +34,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            
             st.subheader(fruit_chosen + ' Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
