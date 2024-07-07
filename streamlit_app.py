338cb92857419eaccfb8b4a20a5cb5420c963e96
Jason Hewitt <jhew@users.noreply.github.com>
2024-07-07 11:19:03 -0400

    add fruitvice nutrition facts tbl

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,11 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen
-    #st.write(ingredients_string)
+            ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
@@ -43,5 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,  {name_on_order}!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
